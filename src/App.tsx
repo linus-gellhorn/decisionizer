dc814dc678db114a7cb14985032e832e4279ebb0
Linus Gellhorn <linus.g@hotmail.com>
2022-02-09 12:16:44 +0000

    Separated out components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import MainContent from "./MainContent";
 
 function App() {
-  return <h1>Decision Making Helper</h1>;
+  return (
+    <>
+      <h1>Decision Making Helper</h1>
+      <MainContent />
+    </>
+  );
 }
 
 export default App;
