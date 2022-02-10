15f12eef8c3ae19e3ba579b2155a0a939d8c227a
Linus Gellhorn <linus.g@hotmail.com>
2022-02-10 11:06:10 +0000

    Removed unused test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import MainContent from "./MainContent";
 import Test from "./Test";
-import Test2 from "./Test2";
 
 function App() {
   return (
     <>
       <h1>Decision Making Helper</h1>
-      {/* <Test2 /> */}
       <Test />
       {/* <MainContent /> */}
     </>
