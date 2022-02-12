058eb4b454c6232d55145b5579caef0d3ada9ada
Linus Gellhorn <linus.g@hotmail.com>
2022-02-12 15:36:43 +0000

    Added slider to attribute node component (non-draggable)

--- src/components/AttributeNode.tsx
+++ src/components/AttributeNode.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { Attribute } from "../types";
 
 function AttributeNode(props: Attribute): JSX.Element {
+  const [weighting, setWeighting] = useState(50);
+
   return (
     <>
       <h3>{props.name}</h3>
-      <h4>Weighting: {props.weighting}</h4>
+      <input
+        type="range"
+        step="10"
+        value={weighting}
+        onChange={(e) => setWeighting(parseInt(e.target.value))}
+      />
+      <h4>Weighting: {weighting / 100}</h4>
     </>
   );
 }
